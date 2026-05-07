dff7c32a77538570de88a0af6eb9cd3966e9af54
Satyam Kumar <satyamkumar629301@gmail.com>
2026-05-07 12:08:45 +0530

    update mongo url

--- server/index.js
+++ server/index.js
@@ -8,13 +8,13 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// Connect to local mongodb
+// Connect to local mongodb.
 mongoose.connect('mongodb://localhost:27017/urlShortener', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
-.then(() => console.log('MongoDB Connected'))
-.catch(err => console.error('MongoDB connection error:', err));
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 
 function generateShortId() {
